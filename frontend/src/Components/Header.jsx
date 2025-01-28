93482290e7b3c9c260cdbd60ff3fb8fd772d9898
OuachouX7 <ouachou9@gmail.com>
2025-01-28 01:07:34 +0100

    some changes

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -1,37 +1,34 @@
 import gpt from "../images/ChatGpt.webp";
 import menubar from "../images/menubar.svg";
 import header from "../images/header.svg";
-import "../Styles/header.css";
 import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className="header-container">
-      <div className="header">
-        <div className="flex justify-between align-center">
-          <p className="flex align-center text-[20px] font-bold">CHATGPT</p>
-          <img
-            className="w-8 h-8 rounded-full ml-2"
-            src={gpt}
-            alt="img"
-          />
+    <div className="w-full flex h-[15vh] justify-center relative">
+      <div className="w-[90%] h-full flex items-center justify-between">
+        <div className="flex justify-between items-center">
+          <p className="text-[20px] font-bold">CHATGPT</p>
+          <img className="w-8 h-8 rounded-full ml-2" src={gpt} alt="img" />
         </div>
-        <div className="nav">
-          <ul>
-            <li id="selected">General</li>
-            <li>Sales</li>
-            <li>Negotiation</li>
-            <li>Marketing</li>
+        <div className="w-1/2 flex flex-col items-center">
+          <ul className="flex justify-between w-[90%] p-1 bg-[#ededed] rounded-[16px] list-none">
+            <li className="text-[#8a8a8a] px-4 py-1">General</li>
+            <li className="text-[#8a8a8a] px-4 py-1">Sales</li>
+            <li className="text-[#8a8a8a] px-4 py-1">Negotiation</li>
+            <li className="text-[#8a8a8a] px-4 py-1">Marketing</li>
           </ul>
         </div>
-        <div className="menu">
+        <div className="flex items-center">
           <img src={menubar} alt="menu" />
-          <div className="Link">
-            <Link to="/home">Home</Link>
+          <div className="ml-2">
+            <Link to="/home" className="text-decoration-none">
+              Home
+            </Link>
           </div>
         </div>
       </div>
-      <img src={header} alt="img2" className="rel" />
+      <img src={header} alt="img2" className="absolute top-0 right-0 z-[-1]" />
     </div>
   );
 };
